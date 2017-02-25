91b6addd014e1b2116d5d1110920b90079fd0eab
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2017-02-25 00:33:46 -1000

    cleaned up routes

--- routes/gallery-routes.js
+++ routes/gallery-routes.js
@@ -16,17 +16,14 @@ function errorMsg(req, res, err) {
   }
 }
 
-function isAuthenticated(req, res, next) { //can change function name
+function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    console.log('pass');
     username = req.user.username;
     loggedIn = true;
     next();
   } else {
-    console.log('NOPE TRY AGAIN');
     req.flash("error-msg", "* please log in first");
     res.redirect(303, '/gallery/login');
-    // next();
   }
 }
 
@@ -39,19 +36,6 @@ function getUsername(req, res, next) {
     next();
   }
 }
-
-// router.get('/', getUsername, (req, res) => {
-//   Gallery.findAll()
-//   .then(function(photos) {
-//     let mainPhoto = photos.splice(0, 1)[0].dataValues;
-//     res.render('index', {
-//       mainPhoto : mainPhoto,
-//       photos : photos,
-//       username : username,
-//       loggedIn : loggedIn
-//     });
-//   });
-// });
 
 router.get('/', getUsername, (req, res) => {
   Gallery.findAll( {
@@ -67,7 +51,6 @@ router.get('/', getUsername, (req, res) => {
     });
   });
 });
-
 
 router.get('/new', isAuthenticated, (req, res) => {
   Gallery.findAll()
@@ -93,10 +76,6 @@ router.get('/login', (req, res) => {
   });
 });
 
-router.get('/secret', isAuthenticated, (req, res) => {
-  res.send('this is my secret page');
-});
-
 router.post('/login',
   passport.authenticate('local',  {
     successRedirect: '/gallery',
@@ -104,33 +83,11 @@ router.post('/login',
     failureFlash: true })
 );
 
-// router.post('/login', function(req, res, next) {
-//   passport.authenticate('local', function(err, user, info) {
-//     if (err) {
-//       return next(err);
-//     }
-//     if (!user) {
-//       return res.redirect('/gallery/login');
-//     }
-//     req.logIn(user, function(err) {
-//       if (err) {
-//         return next(err);
-//       }
-//       username = user.username;
-//       loggedIn = true;
-//       return res.redirect('/gallery');
-//     });
-//   })(req, res, next);
-// });
-
-
 router.get('/logout', isAuthenticated, (req, res) => {
-  console.log('logging out');
   loggedIn = false;
   req.logout();
   res.redirect('/gallery');
 });
-
 
 router.get('/newuser', (req, res) => {
   res.render('newuser', {
@@ -144,7 +101,6 @@ router.post('/newuser', (req, res) => {
     password: req.body.password
   })
   .then(function() {
-    // res.redirect('/gallery');
     req.flash("error-msg", "* SUCCESS!! Please log in");
     res.redirect('/gallery/login');
   })
@@ -170,7 +126,6 @@ router.post('/', isAuthenticated, (req, res) => {
 });
 
 router.get('/:id', getUsername, (req, res) => {
-  // Gallery.find( { where: { id : `${req.params.id}` } })
   Gallery.findAll( {
     order : [['updatedAt', 'DESC']]
   })
@@ -191,15 +146,6 @@ router.get('/:id', getUsername, (req, res) => {
 });
 
 router.get('/:id/edit', isAuthenticated, (req, res) => {
-  // Gallery.findById(`${req.params.id}`)
-  // .then(function(photos) {
-  //   res.render('edit', {
-  //     photos : photos,
-  //     username : username,
-  //     loggedIn : loggedIn,
-  //     messages: res.locals.messages()
-  //   });
-  // });
   Gallery.findAll( {
     order : [['updatedAt', 'DESC']]
   })
@@ -218,7 +164,6 @@ router.get('/:id/edit', isAuthenticated, (req, res) => {
       messages : res.locals.messages()
     });
   });
-
 });
 
 router.put('/:id', isAuthenticated, (req, res) => {
@@ -237,14 +182,12 @@ router.put('/:id', isAuthenticated, (req, res) => {
   })
   .catch(err => {
     let id = req.params.id;
-    console.log('YOU SUCKKKKK');
     errorMsg(req, res, err);
     res.redirect(303, `/gallery/${id}/edit/`);
   });
 });
 
 router.delete('/:id', isAuthenticated, (req, res) => {
-  console.log("DELETED");
   Gallery.destroy( {
     where : {
       id: `${req.params.id}`
@@ -255,6 +198,5 @@ router.delete('/:id', isAuthenticated, (req, res) => {
     res.redirect(303, '/gallery');
   });
 });
-
 
 module.exports = router;
